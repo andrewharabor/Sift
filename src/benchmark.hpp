35f9ba4f6565c03ad876f30f64638a23d6d69df7
Andrew Harabor <andrew.harabor@gmail.com>
2026-04-09 22:16:18 -0700

    Int fix

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -11,7 +11,7 @@ namespace Clownfish {
 class Benchmark {
 public:
     template<MoveGenType MGT = MoveGenType::ALL>
-    static UInt64 perft(Position &position, UInt16 depth) {
+    static UInt64 perft(Position &position, UInt32 depth) {
         MoveList moveList;
 
         if (depth == 1) {
