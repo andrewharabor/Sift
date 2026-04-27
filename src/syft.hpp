38bf178811b440955dc2a3b559c2e6f18b78bf56
Andrew Harabor <andrew.harabor@gmail.com>
2026-04-26 20:55:47 -0700

    PeSTO eval

--- src/syft.hpp
+++ src/syft.hpp
@@ -7,7 +7,7 @@ namespace Syft {
 namespace ID {
 
 constexpr std::string_view NAME = "Syft";
-constexpr std::string_view VERSION = "dev1-position";
+constexpr std::string_view VERSION = "dev2-pesto";
 constexpr std::string_view AUTHOR = "andrewharabor";
 constexpr std::string_view GITHUB_URL = "https://github.com/andrewharabor/Syft";
 
