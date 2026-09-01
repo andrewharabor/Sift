bb28444c20213e31a690f9b7fa6363267c69dee6
Andrew Harabor <andrew.harabor@gmail.com>
2026-08-31 20:22:50 -0700

    clean up optimism formula

--- src/eval.hpp
+++ src/eval.hpp
@@ -19,7 +19,9 @@ public:
 
     static Int32 adjust(Int32 score, const Position &position, const std::array<Int32, 2> &optimism, Int32 correction = 0) noexcept {
         const Int32 material = EVAL_ADJUST_PAWN_SCALE * position.pieces(PieceType::PAWN).count() + EVAL_ADJUST_KNIGHT_SCALE * position.pieces(PieceType::KNIGHT).count() + EVAL_ADJUST_BISHOP_SCALE * position.pieces(PieceType::BISHOP).count() + EVAL_ADJUST_ROOK_SCALE * position.pieces(PieceType::ROOK).count() + EVAL_ADJUST_QUEEN_SCALE * position.pieces(PieceType::QUEEN).count();
-        score = (score * (EVAL_ADJUST_MATERIAL_BASE + material) + optimism[static_cast<USize>(position.sideToMove())] * (EVAL_ADJUST_OPTIMISM_SCALE_BASE + EVAL_ADJUST_OPTIMISM_SCALE_MATERIAL_SCALE * material / 1024)) / 32768;
+        const Int32 materialScale = EVAL_ADJUST_MATERIAL_BASE + material;
+        const Int32 optimismBonus = optimism[static_cast<USize>(position.sideToMove())] * (EVAL_ADJUST_OPTIMISM_SCALE_BASE + EVAL_ADJUST_OPTIMISM_SCALE_MATERIAL_SCALE * material / 1024);
+        score = (score * materialScale + optimismBonus) / 32768;
         score = score * (EVAL_ADJUST_HALF_MOVE_SCALE - static_cast<Int32>(position.halfmoveClock())) / EVAL_ADJUST_HALF_MOVE_SCALE;
         score += correction;
         score = std::clamp(score, Score::LOSS + 1, Score::WIN - 1);
