4339eef26699b1ee493392d80f0d7df594fea967
Andrew Harabor <andrew.harabor@gmail.com>
2026-09-13 13:22:49 -0700

    adjust net arch for current TI net

--- src/nnue.hpp
+++ src/nnue.hpp
@@ -27,32 +27,81 @@ namespace Sift {
 
 namespace NetConfig {
 
+// constexpr USize L1_SIZE = 1024;
+// constexpr USize L2_SIZE = 32;
+// constexpr USize L3_SIZE = 64;
+
+// constexpr Int32 FT_SCALE_BITS = 7;
+// constexpr Int32 FT_QUANT_BITS = 8;
+// constexpr Int32 L1_QUANT_BITS = 7;
+
+// constexpr Int32 SCALE = 400;
+
+// constexpr bool DUAL_ACTIVATION = true;
+// constexpr bool SKIP_L2 = true;
+
+// using PSQFeatureSet = MergedMirroredKingBucketInputs<
+//     MirroredKingSide::ABCD,
+//     0, 1, 2, 3,
+//     4, 5, 6, 7,
+//     8, 9, 10, 11,
+//     8, 9, 10, 11,
+//     12, 12, 13, 13,
+//     12, 12, 13, 13,
+//     14, 14, 15, 15,
+//     14, 14, 15, 15
+// >;
+
+// using InputFeatureSet = PawnPawnThreatInputs<PSQFeatureSet>;
+
+// using Updates = InputFeatureSet::Updates;
+
+// using FeatureTransformer = Sift::FeatureTransformer<L1_SIZE, InputFeatureSet>;
+// using PSQAccumulator = FeatureTransformer::PSQAccumulator;
+// using RefreshTable = FeatureTransformer::RefreshTable;
+
+// using Output = MaterialCountBucketOutput<8>;
+
+// using Arch = PairwiseMultilayerArch<
+//     InputFeatureSet,
+//     L1_SIZE,
+//     L2_SIZE,
+//     L3_SIZE,
+//     FT_SCALE_BITS,
+//     FT_QUANT_BITS,
+//     L1_QUANT_BITS,
+//     DUAL_ACTIVATION,
+//     SKIP_L2,
+//     Output,
+//     SCALE
+// >;
+
 constexpr USize L1_SIZE = 1024;
 constexpr USize L2_SIZE = 32;
-constexpr USize L3_SIZE = 64;
+constexpr USize L3_SIZE = 32;
 
 constexpr Int32 FT_SCALE_BITS = 7;
 constexpr Int32 FT_QUANT_BITS = 8;
-constexpr Int32 L1_QUANT_BITS = 7;
+constexpr Int32 L1_QUANT_BITS = 3;
 
-constexpr Int32 SCALE = 400;
+constexpr Int32 SCALE = 253;
 
-constexpr bool DUAL_ACTIVATION = true;
-constexpr bool SKIP_L2 = true;
+constexpr bool DUAL_ACTIVATION = false;
+constexpr bool SKIP_L2 = false;
 
 using PSQFeatureSet = MergedMirroredKingBucketInputs<
     MirroredKingSide::ABCD,
     0, 1, 2, 3,
     4, 5, 6, 7,
-    8, 9, 10, 11,
-    8, 9, 10, 11,
+    8, 8, 9, 9,
+    10, 10, 11, 11,
     12, 12, 13, 13,
     12, 12, 13, 13,
     14, 14, 15, 15,
-    14, 14, 15, 15
+    14, 14, 15, 15,
 >;
 
-using InputFeatureSet = PawnPawnThreatInputs<PSQFeatureSet>;
+using InputFeatureSet = ThreatInputs<PSQFeatureSet>;
 
 using Updates = InputFeatureSet::Updates;
 
